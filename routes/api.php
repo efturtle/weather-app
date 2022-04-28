41ae0d8a919f1b054b71a98bc8e322d42948cc78
beto cuevas <46721097+efturtle@users.noreply.github.com>
2022-04-28 12:07:07 -0500

    weather model and open weather api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('weather', function(){
+    $time = now()->subDays(5)->timestamp;
+    // get weather from 5 days ago
+    $response = Http::acceptJson()->get("https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=51.5085&lon=-0.1257&dt={$time}&appid={app_id}");
+
+    // daily reports of weather
+    $response = Http::acceptJson()->get("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=currently,minutely,hourly,alerts&appid={app_id}");
+    return $response;
 });
